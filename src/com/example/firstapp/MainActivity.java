483bd1525a2796764bd3d764a7ef8a3effb49a7b
danny_chang <danny_chang@htc.com>
2014-10-21 19:13:56 +0800

    implement on_click function of button
    implement on_click function for get string from EditText and print that
    string in TextView

--- src/com/example/firstapp/MainActivity.java
+++ src/com/example/firstapp/MainActivity.java
@@ -2,19 +2,43 @@ package com.example.firstapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
+	
+	private TextView textView;
+	private EditText editText;
+	private Button button;
+	
 	
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        textView = (TextView) findViewById(R.id.textView1);
+        editText = (EditText) findViewById(R.id.editText1);
+        button = (Button) findViewById(R.id.button1);
+        
+        textView.setText("Hi the World!");
+        button.setText("Submit");
     }
 
+    public void click_Button(View view){
+    
+    	Log.d("debug", "on click");
+    	
+    	String newString = editText.getText().toString();
+    	textView.setText(newString);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
